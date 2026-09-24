bce6766411bbf7b49491d416afaa8b3441f42527
SeuNomeDeUsuarioNoGithub <araujoisaias58@gmail.com>
2026-09-23 21:08:59 -0300

    ENUMS DE USER CRIADOS

---
+++ src/main/java/arajou/br/com/help_desk/user/model/enums/Department.java
@@ -0,0 +1,8 @@
+package arajou.br.com.help_desk.user.model.enums;
+
+public enum Department {
+    HR,
+    MARKETING,
+    IT,
+    PURCHASING
+}
---
+++ src/main/java/arajou/br/com/help_desk/user/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package arajou.br.com.help_desk.user.model.enums;
+
+public enum UserRole {
+    CUSTOMER,
+    TECHNICIAN,
+    MENAGER
+}
